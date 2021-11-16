e7543971f995d3b8ee1220ca3952edbd2629e060
Nicolas Sauvage <nicolas.sauvage@talan.com>
2021-11-16 18:53:24 +0100

    add dockerfile

--- config/prod.exs
+++ config/prod.exs
@@ -10,8 +10,9 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :retrospector, RetrospectorWeb.Endpoint,
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  url: [host: "localhost", port: 4000],
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  server: true
 
 # Do not print debug messages in production
 config :logger, level: :info
